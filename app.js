8407ec344367ae961f038491f6678a39fd7f9bf7
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-10-07 20:23:42 +0200

    exo-2

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-require('dotenv').config();
-
+const connection = require('./db-config');
 const express = require('express');
-
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+connection.connect((err) => {
+  if (err)  {
+    console.error('error connection: ' + err.stack);
+  } else {
+    console.log('Connected to database with thredId: ' + connection.threadId);
+  }
 });
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get('/api/series', (req,res) => {
+  connection.query('SELECT * FROM series', (err,result) => {
+    err ? res.status(500).json('Error retrieving data from database') : res.status(200).json(result);
+  })
+})
