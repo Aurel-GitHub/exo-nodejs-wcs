5313a5b6d5888a631987ecd187a4aec60e6d5b06
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-10-17 17:45:33 +0200

    end exo 9

--- app.js
+++ app.js
@@ -13,102 +13,11 @@ connection.connect((err) => {
     console.log("Connected to database with thredId: " + connection.threadId);
   }
 });
+
 app.use(express.json());
 
 setupRoutes(app);
 
-app.get("/api/users", (req, res) => {
-  connection.query("SELECT * FROM users", (err, result) => {
-    err
-      ? res.status(500).json("Error retrieving data from database")
-      : res.status(200).json(result);
-  });
-});
-
-app.get("/api/users/:id", (req, res) => {
-  const userId = req.params.id;
-  connection.query(
-    "SELECT * FROM users WHERE id = ?",
-    [userId],
-    (err, results) => {
-      if (err) {
-        res.status(500).send("Error retrieving user from database");
-      } else {
-        if (results.length) res.json(results[0]);
-        else res.status(404).send("User not found");
-      }
-    }
-  );
-});
-
-app.post('/api/users', (req, res) => {
-  const { firstname, lastname, email} = req.body;
-  const db = connection.promise();
-  let validationErrors = null;
-  db.query('SELECT * FROM users WHERE email = ?', [email])
-    .then(([result]) => {
-      if (result[0]) return Promise.reject('DUPLICATE_EMAIL');
-      validationErrors = Joi.object({
-        email: Joi.string().email().max(255).required(),
-        firstname: Joi.string().max(255).required(),
-        lastname: Joi.string().max(255).required(),
-      }).validate({ firstname, lastname, email }, { abortEarly: false }).error;
-      if (validationErrors) return Promise.reject('INVALID_DATA');
-      return db.query(
-        'INSERT INTO users (firstname, lastname, email) VALUES (?, ?, ?)',
-        [firstname, lastname, email,]
-      );
-    })
-    .then(([{ insertId }]) => {
-      res.status(201).json({ id: insertId, firstname, lastname, email });
-    })
-    .catch((err) => {
-      console.error(err);
-      if (err === 'DUPLICATE_EMAIL')
-        res.status(409).json({ message: 'This email is already used' });
-      else if (err === 'INVALID_DATA')
-        res.status(422).json({ validationErrors });
-      else res.status(500).send('Error saving the user');
-    });
-});
-
-app.put('/api/users/:id', (req, res) => {
-  const userId = req.params.id;
-  const { firstname, lastname, email } = req.body;
-  const db = connection.promise();
-  let existingUser = null;
-  let validationErrors = null;
-  db.query('SELECT * FROM users WHERE id = ?', [userId])
-    .then(([results]) => {
-      validationErrors = Joi.object({
-        email: Joi.string().email().max(255),
-        firstname: Joi.string().max(255),
-        lastname: Joi.string().max(255),
-      }).validate({ firstname, lastname, email }, { abortEarly: false }).error;
-      existingUser = results[0];
-      if (!existingUser) return Promise.reject('RECORD_NOT_FOUND');
-      if (validationErrors) return Promise.reject('INVALID_DATA');
-      return db.query('UPDATE users SET ? WHERE id = ?', [req.body, userId]);
-    })
-    .then(() => {
-      res.status(200).json({ ...existingUser, ...req.body });
-    })
-    .catch((err) => {
-      console.error(err);
-      if (err === 'RECORD_NOT_FOUND')
-        res.status(404).send(`User with id ${userId} not found.`);
-      else res.status(500).send('Error updating a user');
-    });
-});
-
-app.delete("/api/users/:id", (req, res) => {
-  const userId = req.params.id;
-  connection.query("DELETE FROM users WHERE id = ?", [userId], (err) => {
-    err
-      ? res.status(500).send("Error deleting an user").json(err)
-      : res.status(200).send("User deleted!");
-  });
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,6 +2,78 @@ const usersRouter = require('express').Router();
 const Users = require('../models/users/users.model');
 
 usersRouter.get('/', (req, res) => {
-    
-})
+    Users.findMany({ filters: {firstname, lastname}})
+        .then((users) => {
+            res.json(users);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send('Error retrieving users from database');
+        });
+});
+
+usersRouter.get('/:id', (req, res) => {
+    Users.findOne(req.params.id)
+        .then((user) => {
+            user ? res.json(user) : res.status(400).send('User not found')
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send('Error retrieving user from database');
+        });
+});
+
+usersRouter.post('/', (req, res) => {
+    const error = Users.validate(req.body);
+    error 
+    ? res.status(422).json({validationErrors: error.details })
+    : Users.create(req.body)
+        .then((createUser) => {
+            res.status(201).json(createUser);
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).send('Error saving the user');
+        });
+});
+
+usersRouter.put(':/id', (req,res) => {
+    let existingUser = null;
+    let validationErrors = null;
+    Users.findOne(req.params.id)
+        .then((user) => {
+            existingUser = user;
+            if (!existingUser) return Promise.reject('RECORD_NOT_FOUND');
+            validationErrors = Users.validate(req.body, false);
+            if (existingUser) return Promise.reject('INVALID_DATA');
+            return Users.update(req.params.id, req.body);
+        })
+        .then(() => { 
+            res.status(200).json({ ...existingUser, ...req.body });
+        })
+        .catch((err) => {
+            console.log(err);
+            if (err === 'RECORD_NOT_FOUND')
+                res.status(404).send(`User with ${req.params.id} not found.`);
+            else if (err === 'INVALID_DATA')
+                res.status(422).json({ validationErrors: validationErrors.details });
+            else 
+                res.status(500).send('Error updating a user');
+        });
+});
+
+usersRouter.delete('/:id', (req,res) => {
+    Users.destroy(req.params.id)
+        .then((deleted) => {
+            deleted ? res.status(200).send('User deleted') : res.status(404).send('User not found')
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send('Error deleting a user');
+        });
+});
+
+
+
+
 module.exports = usersRouter;
